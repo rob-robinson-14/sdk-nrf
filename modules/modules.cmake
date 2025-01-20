dcf82723907333eb968cb2497609eed6d6bce0c6
Robinson, Robert <robert.robinson@nordicsemi.no>
2025-01-20 17:35:19 +0000

    modules: hal_nordic: Point to release candidate 3.10 NRFX
    NRFX 3.10 not yet released to public so intneral instance required for
    all 7120 MDK files
    
    Signed-off-by: Robert Robinson <robert.robinson@nordicsemi.no>

--- modules/modules.cmake
+++ modules/modules.cmake
@@ -7,6 +7,7 @@ set(ZEPHYR_COREMARK_KCONFIG   ${CMAKE_CURRENT_LIST_DIR}/coremark/Kconfig)
 set(ZEPHYR_TRUSTED_FIRMWARE_M_KCONFIG ${CMAKE_CURRENT_LIST_DIR}/trusted-firmware-m/Kconfig)
 set(ZEPHYR_AZURE_SDK_FOR_C_KCONFIG ${CMAKE_CURRENT_LIST_DIR}/azure-sdk-for-c/Kconfig)
 set(ZEPHYR_AZURE_SDK_FOR_C_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR}/azure-sdk-for-c)
+set(ZEPHYR_HAL_NORDIC_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR}/hal_nordic)
 
 # Those are modules with Kconfig tree's inside the module repo but where
 # nRF Connect SDK extend those trees.
