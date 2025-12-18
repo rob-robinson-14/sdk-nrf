32c2efd58b9a796f9c7b7269500cff44ce6d69eb
Robert Robinson <robert.robinson@nordicsemi.no>
2025-12-18 16:12:27 +0000

    soc: Move nRF7120 support to upstream zephyr.
    Support for nrf7120 has been upstreamed to Zephyr and
    so can now be removed from sdk-nrf.
    
    Signed-off-by: Robert Robinson <robert.robinson@nordicsemi.no>

--- soc/nordic/nrf71/soc_preeng.h
+++ soc/nordic/nrf71/soc_preeng.h
@@ -8,17 +8,14 @@
  * @file SoC configuration macros for the Nordic Semiconductor NRF71 family processors.
  */
 
-#ifndef _NORDICSEMI_NRF71_SOC_H_
-#define _NORDICSEMI_NRF71_SOC_H_
+#ifndef _NORDICSEMI_NRF71_PREENG_SOC_H_
+#define _NORDICSEMI_NRF71_PREENG_SOC_H_
 
 #include <soc_nrf_common.h>
-
-#define FLASH_PAGE_ERASE_MAX_TIME_US 42000UL
-#define FLASH_PAGE_MAX_CNT	     381UL
 
 #ifdef CONFIG_SOC_NRF7120_PREENG
 int configure_playout_capture(uint32_t rx_mode, uint32_t tx_mode, uint32_t rx_holdoff_length,
 			      uint32_t rx_wrap_length, uint32_t back_to_back_mode);
 #endif /* CONFIG_SOC_NRF7120_PREENG */
 
-#endif /* _NORDICSEMI_NRF71_SOC_H_ */
+#endif /* _NORDICSEMI_NRF71_PREENG_SOC_H_ */
